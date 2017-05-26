b3b75e1229742395cffb729e8c24226203ca3466
Kevin Wang <kwang2@stanford.edu>
2017-05-25 17:46:34 -0700

    finished recursion

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,22 @@
 
 # Lab 16: recursion
 # Fibonacci with cache
+
+cache = {} # n => fib(n)
+
+def fib(n):
+    # base case
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n in cache:
+        return cache[n]
+    else:
+        fib_sum = fib(n - 1) + fib(n - 2)
+        cache[n] = fib_sum
+        return fib_sum
+    
+    
+
+print("fib(40):", fib(40))
--- levenshtein.py
+++ levenshtein.py
@@ -2,3 +2,30 @@
 
 # Lab 16: Recursion
 # Levenshtein Distance
+
+
+def lev(a, b):
+    if not a: # base case
+        return len(b)
+    elif not b:
+        return len(a)
+    else: # recursive case
+        if a[-1] == b[-1]:
+            return lev(a[0:-1], b[0:-1])
+        else:
+            dis1 = lev(a[0:-1], b[0:-1])
+            dis2 = min(lev(a, b[0:-1]), lev(a[0:-1], b))
+            return min(dis1, dis2) + 1
+        
+
+
+
+
+
+a = "armchair"
+b = "armhair"
+print ('test1: lev(%s, %s) = %d' % (a, b, lev(a, b)))
+
+a = "physics"
+b = "psychics"
+print ('test2: lev(%s, %s) = %d' % (a, b, lev(a, b)))
